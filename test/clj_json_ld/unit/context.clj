e6f4f4d41ea2ffb638dc8786e8b63bccf2bf4c98
Sean Johnson <sean@snootymonkey.com>
2014-11-05 11:23:10 -0500

    Unsaved changes.

--- test/clj_json_ld/unit/context.clj
+++ test/clj_json_ld/unit/context.clj
@@ -1,3 +1,2 @@
 (ns clj-json-ld.unit.context
-  (:require [midje.sweet :refer :all]
-            [clj-json-ld.context :as context]))
+  (:require [midje.sweet :refer :all]))
--- test/clj_json_ld/unit/value.clj
+++ test/clj_json_ld/unit/value.clj
@@ -4,6 +4,4 @@
 
   Test value compaction as defined here: http://www.w3.org/TR/json-ld-api/#iri-compaction
   "
-  (:require [midje.sweet :refer :all]
-            [clj-json-ld.json-ld :as json-ld]
-            [clj-json-ld.iri :refer (expand-iri)]))
+  (:require [midje.sweet :refer :all]))
