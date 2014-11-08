ada0e3b5f330e54e8f3d5b6800a55a8707b4cc3b
Sean Johnson <sean@snootymonkey.com>
2014-11-08 10:08:38 -0500

    WIP on context processing algorithm.

--- src/clj_json_ld/context.clj
+++ src/clj_json_ld/context.clj
@@ -1,1 +1,27 @@
-(ns clj-json-ld.context)
+(ns clj-json-ld.context
+  "
+  Implement context processing as defined in the
+  [Context Processing Algorithms section](http://www.w3.org/TR/json-ld-api/#context-processing-algorithm).
+  "
+  (:require [defun :refer (defun)]))
+
+(defun update-with-local-context 
+  "Update an active context with a local context."
+  
+  ;; If remote contexts is not passed, it is initialized to an empty array.
+  ([active-context local-context]
+    (update-with-local-context active-context local-context []))
+  
+  ;; 2) If local context is not an array, set it to an array containing only local context.
+  ([active-context local-context :guard #(not (vector? %)) remote-contexts]
+    (update-with-local-context active-context [local-context] remote-contexts))
+
+  ;; Recursion for:
+  ;; 3) For each item context in local context
+
+  ([active-context [] remote-contexts]
+    (println "empty!"))
+
+  ([active-context local-context remote-contexts]
+    (println "NOT empty!" (first local-context))
+    (update-with-local-context active-context (vec (rest local-context)) remote-contexts)))
--- test/clj_json_ld/unit/context.clj
+++ test/clj_json_ld/unit/context.clj
@@ -1,2 +1,5 @@
 (ns clj-json-ld.unit.context
+  "
+  Test context processing as defined here: http://www.w3.org/TR/json-ld-api/#context-processing-algorithm
+  "
   (:require [midje.sweet :refer :all]))
