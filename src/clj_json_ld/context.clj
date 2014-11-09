8b92839b96034427ccf57f75aeda13e226ec0174
Sean Johnson <sean@snootymonkey.com>
2014-11-09 08:16:00 -0500

    Remove stray requires.

--- src/clj_json_ld/context.clj
+++ src/clj_json_ld/context.clj
@@ -4,8 +4,7 @@
   [Context Processing Algorithms section](http://www.w3.org/TR/json-ld-api/#context-processing-algorithm).
   "
   (:require [defun :refer (defun)]
-            [clojure.core.match :refer (match)]
-            [clj-json-ld.JSONLDError :refer :all]))
+            [clojure.core.match :refer (match)]))
 
 (defun update-with-local-context 
   "Update an active context with a local context."
@@ -27,18 +26,26 @@
   ([active-context local-context remote-contexts]
     (let [context (first local-context)]
       (match [context]
+
         [nil] 
-          ;; 3.1) If context is null, set result to a newly-initialized active context and continue with the next context. The base IRI of the active context is set to the IRI of the currently being processed document (which might be different from the currently being processed context), if available; otherwise to null. If set, the base option of a JSON-LD API Implementation overrides the base IRI.
+          ;; 3.1) If context is null, set result to a newly-initialized active context and continue
+          ;; with the next context. The base IRI of the active context is set to the IRI of the
+          ;; currently being processed document (which might be different from the currently being
+          ;; processed context), if available; otherwise to null. If set, the base option of a JSON-LD
+          ;; API Implementation overrides the base IRI.
           (println "Nothing!")
+
         [string-context :guard #(string? %)]
           ;; 3.2) If context is a string
           (println "String!")
+
         [map-context :guard #(not (map? %))] 
           ;; TODO Use a real Java exception here, not ex-info
           ;; 3.3) If context is NOT a JSON object, an invalid local context error has been detected and
           ;; processing is aborted.
           (throw (ex-info "JSONLDError" {:code "invalid local context"
             :message "local context is not a JSON object"}))
+
         [_] 
           ;; 3.4, 3.5, 3.6) If context IS a JSON object, process the context
           (println "Context processing!"))
