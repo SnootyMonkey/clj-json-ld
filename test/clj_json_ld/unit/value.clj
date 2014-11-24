e8d6f1815ef1bf190ed1ac0db3eb8971c149f5a8
Sean Johnson <sean@snootymonkey.com>
2014-11-24 08:54:50 -0500

    Code comment cleanup.

--- test/clj_json_ld/unit/value.clj
+++ test/clj_json_ld/unit/value.clj
@@ -22,17 +22,6 @@
 
 (facts "about value expansion"
 
-  ;; Email sent to JSON-LD list on Nov. 7th to determine if we should be
-  ;; doing this. Don't see it in the spec. Maybe it's part of the RDF stuff?
-
-  ;; Translation: if the value is literally a Ruby type, then expand
-  ;; it to an IRI pointing to the W3C XML Schema IRI for the type
-  ; %w(boolean integer string dateTime date time).each do |dt|
-  ;    it "expands datatype xsd:#{dt}" do
-  ;      expect(subject.expand_value("foo", RDF::XSD[dt])).to produce({"@id" => "http://www.w3.org/2001/XMLSchema##{dt}"}, @debug)
-  ;    end
-  ;  end
-
   (facts "with no language mapping"
     (fact "absolute IRI"
       (expand-value context "foaf:knows" "http://example.com/") => {"@id" "http://example.com/"})
@@ -67,11 +56,6 @@
     (fact "native double"
       (expand-value context "foo" 1.1e1) => {"@value" 1.1E1})
 
-    ;; Our tests here differ from the Ruby lib's tests.
-    ;; Email sent to JSON-LD list on Nov. 7th to determine if we should be
-    ;; adding @type based on the native date/time/dateTime type, or only
-    ;; based on the active context. Maybe it's part of the RDF stuff?
-
     (fact "native date"
       (let [date-stamp (.parse (java.text.SimpleDateFormat. "yyyy-MM-dd") "2011-12-27")]
         (expand-value context "foo", date-stamp) => {"@value" date-stamp}))
@@ -83,18 +67,6 @@
     (fact "native dateTime"
       (let [date-time-stamp (.parse (java.text.SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss") "2011-12-27T10:11:12Z")]
         (expand-value context "foo" date-time-stamp) => {"@value" date-time-stamp})))
-
-    ;; Maybe wait on these until dealing with RDF? Maybe we don't even have this case
-    ;; given how we'll be handling RDF?
-
-    ;   "rdf boolean" =>    ["foo", RDF::Literal(true),             {"@value" => "true", "@type" => RDF::XSD.boolean.to_s}],
-    ;   "rdf integer" =>    ["foo", RDF::Literal(1),                {"@value" => "1", "@type" => RDF::XSD.integer.to_s}],
-    ;   "rdf decimal" =>    ["foo", RDF::Literal::Decimal.new(1.1), {"@value" => "1.1", "@type" => RDF::XSD.decimal.to_s}],
-    ;   "rdf double" =>     ["foo", RDF::Literal::Double.new(1.1),  {"@value" => "1.1E0", "@type" => RDF::XSD.double.to_s}],
-    ;   "rdf URI" =>        ["foo", RDF::URI("foo"),                {"@id" => "foo"}],
-    ;   "rdf date " =>      ["foo", RDF::Literal(Date.parse("2011-12-27")), {"@value" => "2011-12-27", "@type" => RDF::XSD.date.to_s}],
-    ;   "rdf nonNeg" =>     ["foo", RDF::Literal::NonNegativeInteger.new(1), {"@value" => "1", "@type" => RDF::XSD.nonNegativeInteger}],
-    ;   "rdf float" =>      ["foo", RDF::Literal::Float.new(1.0), {"@value" => "1.0", "@type" => RDF::XSD.float}],
 
   (facts "with a default @language mapping of en"
 
